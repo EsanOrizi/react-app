8c9936953798931c75e77ff2374a93bc5cfa3a72
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2023-04-16 22:00:32 +0100

    Passing Functions via Props

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,12 @@ function App() {
     'London',
     'Paris'
   ];
-  return <div><ListGroup items={items} heading="Cities" /></div>;
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+
+  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} /></div>;
 }
 
 export default App;
