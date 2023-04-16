a2a9999a2d03fa12f287f95e2462d5afd442516e
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2023-04-16 22:19:36 +0100

    Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import Message from "./components/ListGroup";
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+
 
 function App() {
-  let items = [
-    'New York',
-    'London',
-    'Paris'
-  ];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
-
-  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} /></div>;
+    return <div>
+        <Alert><span>Hello World</span></Alert>
+    </div>;
 }
 
 export default App;
